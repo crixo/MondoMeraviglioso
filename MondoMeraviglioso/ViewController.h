4ac57ff6bce75f6fb1ab58cbe21df6feff507b33
cristiano-degiorgis <cri@webprofessor.it>
2013-10-29 20:33:16 +0100

    Initial Commit

---
+++ MondoMeraviglioso/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MondoMeraviglioso
+//
+//  Created by Cristiano Degiorgis on 29/10/13.
+//  Copyright (c) 2013 WebProfessor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
